7ef6767e45730ea8ac035647266a12ce4f43d2d9
Luis Angel Cervantes Cortes <la.cervantes10@uniandes.edu.co>
2015-02-05 16:59:57 -0500

    Taller básico Javascript

---
+++ public_html/scripts/taller.js
@@ -0,0 +1,65 @@
+function calcular() {
+    var x, y;
+    x = document.getElementById("valor1").value;
+    y = document.getElementById("valor2").value;
+
+    if (isNaN(x)) {
+        document.getElementById("msj").innerHTML = "<div class=\"alert alert-danger\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>El valor 1 debe ser un número</div>";
+    }
+    else if (isNaN(y)) {
+        document.getElementById("msj").innerHTML = "<div class=\"alert alert-danger\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>El valor 2 debe ser un número</div>";
+    }
+    else
+    {
+        var rta = parseInt(x) + parseInt(y);
+        document.getElementById("msj").innerHTML = "<div class=\"alert alert-success\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>La respuesta es: " + rta + "</div>";
+        ;
+    }
+}
+
+function concatenar() {
+    var x, y;
+    x = document.getElementById("valor1").value;
+    y = document.getElementById("valor2").value;
+    var rta = x + y;
+    document.getElementById("msj").innerHTML = "<div class=\"alert alert-success\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>La respuesta es: " + rta + "</div>";
+    ;
+}
+
+function ordenar() {
+    var x, y, z;
+    var mayor;
+    x = document.getElementById("valor1").value;
+    y = document.getElementById("valor2").value;
+    z = document.getElementById("valor3").value;
+    if (!isNaN(x)) {
+        document.getElementById("msj").innerHTML = "<div class=\"alert alert-danger\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>El valor 1 no debe ser un número</div>";
+    }
+    else if (!isNaN(y)) {
+        document.getElementById("msj").innerHTML = "<div class=\"alert alert-danger\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>El valor 2 no debe ser un número</div>";
+    }
+    else if (!isNaN(z)) {
+        document.getElementById("msj").innerHTML = "<div class=\"alert alert-danger\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>El valor 3 no debe ser un número</div>";
+    }
+    else
+    {
+        var bandera;
+        do{
+            bandera = 0;
+            if(x > y){
+                mayor = x;
+                x = y;
+                y = mayor;
+                bandera = 1;
+            }
+            if(y > z){
+                mayor = y;
+                y = z;
+                z = mayor;
+                bandera = 1;
+            }
+        }while(bandera === 1);
+        document.getElementById("msj").innerHTML = "<div class=\"alert alert-success\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>La respuesta es: " + x + y + z +"</div>";
+        ;
+    }
+}
